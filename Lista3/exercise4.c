4c336eff2c6bf36109c5cc1c44f6de02ed09c0c1
[Thales Menezes de Oliveira] <[thales.menezes.de.oliveira@gmail.com]>
2020-05-21 19:51:39 -0300

    exercise4 new

--- Lista3/exercise4.c
+++ Lista3/exercise4.c
@@ -10,10 +10,14 @@
     gStyle->SetOptStat(0);
     mytree->Draw("(px**2 + py**2 + pz**2)**(1/2)",*ebeam_h);
     c1->SaveAs("Ex4_histo_high.pdf");
+    
     TCanvas *c2 = new TCanvas("c2","Tree canvas", 800, 600);
     mytree->Draw("(px**2 + py**2 + pz**2)**(1/2)",*ebeam_l);
     c2->SaveAs("Ex4_histo_low.pdf");
     
+    TCanvas *c3 = new TCanvas("c3","Tree canvas", 800, 600);
+    mytree->Draw("(px**2 + py**2 + pz**2)**(1/2)",*ebeam_l || *ebeam_h);
+    c3->SaveAs("Ex4_histo_both.pdf");
 
     
     
