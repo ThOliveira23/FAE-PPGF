89a56d69f21a5f5296f026051d9db7f4ae5e20a4
[Thales Menezes de Oliveira] <[thales.menezes.de.oliveira@gmail.com]>
2020-06-10 20:22:40 -0300

    main174

--- ListaMCPythia/main176.cc
+++ ListaMCPythia/main176.cc
@@ -4,16 +4,16 @@ using namespace Pythia8;
 int main()  {
     
     Pythia pythia;
-    pythia.readString("Beams:eCM = 8000.");
-    pythia.readString("Higgs:useBSM = on");
-    pythia.readString("HiggsBSM:gg2H1 = on");
-    pythia.readString("HiggsH1:parity = 3");
-    pythia.readString("HiggsH1:etaParity = 0.5");
-    pythia.readString("25:m0 = 1000.0");
-    pythia.readString("25:onMode = off ");
-    pythia.readString("25:onIfMatch = 23 23");
-    pythia.readString("23:onMode = off");
-    pythia.readString("23:onIfAny = 11 13 15");
+    pythia.readString("Beams:eCM = 8000.");  // kdpsa
+    pythia.readString("Higgs:useBSM = on");  // allow BSM Higgs production
+    pythia.readString("HiggsBSM:gg2H1 = on"); // gluon fusion for H1
+    pythia.readString("HiggsH1:parity = 3");   // H1 is a mixing of CP even/odd
+    pythia.readString("HiggsH1:etaParity = 0.5");  // set strength of CP odd
+    pythia.readString("25:m0 = 1000.0");  // H1 mass
+    pythia.readString("25:onMode = off ");  // turn off all H1 decays
+    pythia.readString("25:onIfMatch = 23 23"); // turn on only H1 to ZZ
+    pythia.readString("23:onMode = off");   // turn off all Z decays
+    pythia.readString("23:onIfAny = 11 13 15");  // decays in leptons
     //pythia.readFile("list_of_pythia_parameters.txt");
     pythia.init();
     Hist mult("charged multiplicity", 100, -0.5, 799.5);
